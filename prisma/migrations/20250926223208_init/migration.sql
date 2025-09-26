84c3b68752cbb135e45638983e9995d26c1be21e
sebastianpl1306 <66373285+sebastianpl1306@users.noreply.github.com>
2025-09-26 18:54:11 -0500

    Configuración inicial del proyecto
    * Se realiza la configuración inicial del proyecto
    * Se realiza una configuración inicial de los modelos de la base de datos
    * Se realiza la configuración de docker(Dockerfile, docker-compose, docker-compose-db)

---
+++ prisma/migrations/20250926223208_init/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "public"."Curso" (
+    "id" SERIAL NOT NULL,
+    "nombre" TEXT NOT NULL,
+    "descripcion" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Curso_pkey" PRIMARY KEY ("id")
+);
